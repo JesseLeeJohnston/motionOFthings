c09f67c33c57f2b4539dc1baea18225f270b3a27
jesseleejohnston <jesseleejohnston@gmail.com>
2019-01-02 20:12:45 -0800

    Setting up as a nuxt project.

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+@import '../assets/sass/main.scss'
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div class="grid">
+    <div class="column">Column 1</div>
+    <div class="column">Column 2</div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .grid {
+    display: grid;
+        grid-template-columns: 1fr 1fr;
+
+  }
+  .column {
+  }
+
+</style>
+
+
